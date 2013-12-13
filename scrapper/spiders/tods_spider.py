d28890127bcb3f73fc725c091073cd7dc6f47287
dhcdht <dhcdht@qq.com>
2013-12-13 15:35:28 +0800

    加入tods无网店国家

--- scrapper/spiders/tods_spider.py
+++ scrapper/spiders/tods_spider.py
@@ -31,6 +31,10 @@ class TodsSpider(MFashionSpider):
             'fr': 'http://www.tods.com/fr/',
             'de': 'http://www.tods.com/de/',
             'es': 'http://www.tods.com/es/',
+
+            'cn': 'http://www.tods.com/cn/',
+            'jp': 'http://www.tods.com/jp/',
+            'ke': 'http://www.tods.com/rok/',
         },
     }
 
@@ -46,6 +50,10 @@ class TodsSpider(MFashionSpider):
             'fr': self.parse_left_nav_withshop,
             'de': self.parse_left_nav_withshop,
             'es': self.parse_left_nav_withshop,
+
+            'cn': self.parse_left_nav_withoutshop,
+            'jp': self.parse_left_nav_withoutshop,
+            'kr': self.parse_left_nav_withoutshop,
         }
 
         super(TodsSpider, self).__init__('tod\'s', region)
@@ -137,8 +145,8 @@ class TodsSpider(MFashionSpider):
         sel = Selector(response)
 
         # 解析当前打开的标签的下一级标签，作为第三级tag
-        currentOpendNodes = sel.xpath('//div[@class="leftNavBarfashionList"]//div[contains(@class, "item")]/div[contains(@class, "select")]//a')
-        for node in currentOpendNodes:
+        currentOpenNodes = sel.xpath('//div[@class="leftNavBarfashionList"]//div[contains(@class, "item")]/div[contains(@class, "select")]//a')
+        for node in currentOpenNodes:
             tag_text = ''.join(self.reformat(val) for val in node.xpath('.//text()').extract())
             tag_text = self.reformat(tag_text)
             tag_name = tag_text.lower()
@@ -281,6 +289,142 @@ class TodsSpider(MFashionSpider):
             imageUrls = list(
                 self.process_href(val, response.url)
                 for val in imageNodes.xpath('./@data-imagezoomedurl').extract()
+            )
+
+        item = ProductItem()
+        item['url'] = metadata['url']
+        item['model'] = metadata['model']
+        if imageUrls:
+            item['image_urls'] = imageUrls
+        item['metadata'] = metadata
+
+        yield item
+
+    def parse_left_nav_withoutshop(self, response):
+        """
+        有网店和无网店，左边导航栏结构不同；
+        针对无网店的国家，解析左边导航栏。
+
+        有网店：
+            http://store.tods.com/Tods/GB/categories/Shop-Man/Bags/All/c/221-Tods
+        无网点：
+            http://www.tods.com/cn/woman
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        # 解析当前打开的标签的下一级标签，作为第三级tag
+        currentOpenNodes = sel.xpath('//ul[@id="vert-nav"]/li[@class="second-level-group"][contains(@style, "display")]//a[@title]')
+        for node in currentOpenNodes:
+            tag_text = node.xpath('./text()').extract()[0]
+            tag_text = self.reformat(tag_text)
+            tag_name = tag_text.lower()
+
+            if tag_text and tag_name:
+                m = copy.deepcopy(metadata)
+
+                m['tags_mapping']['category-2'] = [
+                    {'name': tag_name, 'title': tag_text},
+                ]
+
+                href = node.xpath('./@href').extract()[0]
+                href = self.process_href(href, response.url)
+
+                yield Request(url=href,
+                              callback=self.parse_product_list_withoutshop,
+                              errback=self.onerr,
+                              meta={'userdata': m})
+
+        for val in self.parse_product_list_withoutshop(response):
+            yield val
+
+    def parse_product_list_withoutshop(self, response):
+        """
+        针对无网店国家，解析单品列表
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        productNodes = sel.xpath('//div[@class="category-products"]//li/a')
+        for node in productNodes:
+            m = copy.deepcopy(metadata)
+
+            name = node.xpath('./@title').extract()[0]
+            name = self.reformat(name)
+            if name:
+                m['name'] = name
+
+            href = node.xpath('./@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product_withoutshop,
+                          errback=self.onerr,
+                          meta={'userdata': m},
+                          dont_filter=True)
+
+        # 解析右下角的页数链接，包括显示全部
+        pageNodes = sel.xpath('//div[@class="toolbar-bottom"]//li[@class="pager"]//a')
+        for node in pageNodes:
+            href = node.xpath('./@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product_list_withoutshop,
+                          errback=self.onerr,
+                          meta={'userdata': metadata})
+
+    def parse_product_withoutshop(self, response):
+        """
+        针对无网店国家，解析单品页面
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        # 每个颜色的单品它都给了单独的货号，这里从页面上找到各个颜色的链接，递归
+        colorNodes = sel.xpath('//div[contains(@class, "colour_preview")]//a')
+        for node in colorNodes:
+            m = copy.deepcopy(metadata)
+
+            href = node.xpath('./@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product_withoutshop,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+        metadata['url'] = response.url
+
+        # 尝试从页面中取得model
+        model = ''.join(self.reformat(val) for val in sel.xpath('//div[@class="product-main-info"]/p//text()').extract())
+        model = self.reformat(model)
+        if model:
+            metadata['model'] = model
+        else:
+            return
+
+        # 如果metadata中没有name，尝试从页面中找到name
+        if not metadata['name']:
+            name = ''.join(self.reformat(val) for val in sel.xpath('//div[@class="product-name"]/h1/text()').extract())
+            name = self.reformat(name)
+            if name:
+                metadata['name'] = name
+
+        description = '\r'.join(self.reformat(val) for val in sel.xpath('//div[contains(@class, "description")]/div[@class="std"]//text()').extract())
+        description = self.reformat(description)
+        if description:
+            metadata['description'] = description
+
+        imageUrls = None
+        imageNodes = sel.xpath('//div[@class="more-views"]/ul[@class="thumnail-images"]/li/a/img[@data-zoom-image]')
+        if imageNodes:
+            imageUrls = list(
+                self.process_href(val, response.url)
+                for val in imageNodes.xpath('./@data-zoom-image').extract()
             )
 
         item = ProductItem()
