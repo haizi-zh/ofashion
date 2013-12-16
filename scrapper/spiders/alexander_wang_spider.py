2a0ab940e387bc511d368d4ba204116997b4a7e5
dhcdht <dhcdht@qq.com>
2013-12-16 12:12:52 +0800

    alexander_wang的爬虫，中国完成

--- scrapper/spiders/alexander_wang_spider.py
+++ scrapper/spiders/alexander_wang_spider.py
@@ -49,9 +49,9 @@ class AlexanderWangSpider(MFashionSpider):
                 if gender:
                     m['gender'] = [gender]
 
-                subNodes = node.xpath('.//li[child::a[@href]]')
-                for subNode in subNodes:
-                    tag_text = subNode.xpath('./a/text()').extract()[0]
+                sub_nodes = node.xpath('.//li[child::a[@href]]')
+                for sub_node in sub_nodes:
+                    tag_text = sub_node.xpath('./a/text()').extract()[0]
                     tag_text = self.reformat(tag_text)
                     tag_name = tag_text.lower()
 
@@ -62,11 +62,11 @@ class AlexanderWangSpider(MFashionSpider):
                             {'name': tag_name, 'title': tag_text},
                         ]
 
-                        gender = common.guess_gender(gender)
+                        gender = common.guess_gender(tag_name)
                         if gender:
                             mc['gender'] = [gender]
 
-                        href = subNode.xpath('./a/@href').extract()[0]
+                        href = sub_node.xpath('./a/@href').extract()[0]
                         href = self.process_href(href, response.url)
 
                         yield Request(url=href,
@@ -117,6 +117,9 @@ class AlexanderWangSpider(MFashionSpider):
             yield val
 
     def parse_product_list(self, response):
+        """
+        解析单品列表，发送加载更多的请求
+        """
 
         metadata = response.meta['userdata']
         sel = Selector(response)
@@ -131,9 +134,9 @@ class AlexanderWangSpider(MFashionSpider):
             if name:
                 m['name'] = name
 
-            priceNode = node.xpath('.//div[@class="productPrice"]/div[@class="oldprice"]')
-            if priceNode:
-                price = ''.join(self.reformat(val) for val in priceNode.xpath('.//text()').extract())
+            price_node = node.xpath('.//div[@class="productPrice"]/div[@class="oldprice"]')
+            if price_node:
+                price = ''.join(self.reformat(val) for val in price_node.xpath('.//text()').extract())
                 price = self.reformat(price)
                 if price:
                     m['price'] = price
@@ -147,5 +150,129 @@ class AlexanderWangSpider(MFashionSpider):
                 if colors:
                     m['color'] = colors
 
+            # 这个li的node里边，随便一个a标签，都可以到单品页面
+            href = node.xpath('.//a[@href]/@href').extract()[0]
+            href = self.process_href(href, response.url)
 
-        # TODO 页面下拉到底部会自动加载更多，需要模拟请求，解析返回的json
+            yield Request(url=href,
+                          callback=self.parse_product,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+
+        # 页面下拉到底部会自动加载更多，需要模拟请求，解析返回的json
+        # 测试发现，在原有url后边添加 ?page=2 也可以取到第二页内容
+        # 如果当前页有内容，再考虑请求下一页
+        if product_nodes:
+            # 取的当前页数
+            current_page = 1
+            mt = re.search(r'page=(\d+)', response.url)
+            if mt:
+                current_page = (int)(mt.group(1))
+
+            next_page = current_page + 1
+            # 拼下一页的url
+            if mt:
+                next_url = re.sub(r'page=\d+', str.format('page={0}', next_page), response.url)
+            else:
+                next_url = str.format('{0}?page={1}', response.url, next_page)
+
+            # 请求下一页
+            yield Request(url=next_url,
+                          callback=self.parse_product_list,
+                          errback=self.onerr,
+                          meta={'userdata': metadata})
+
+    def parse_product(self, response):
+        """
+        解析单品页面
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        metadata['url'] = response.url
+
+        # 页面中的货号栏，注意前边会有没用的字符（比如 货号：）
+        model = None
+        model_node = sel.xpath('//li[@id="description_container"]/div[@id="description_pane"]/div[@class="style"]')
+        if model_node:
+            model_text = model_node.xpath('./text()').extract()[0]
+            model_text = self.reformat(model_text)
+            if model_text:
+                mt = re.search(r'\b(\w+)\b', model_text)
+                if mt:
+                    model = mt.group(1)
+
+        if model:
+            metadata['model'] = model
+        else:
+            return
+
+
+        colors = [
+            self.reformat(val)
+            for val in sel.xpath('//div[@class="itemColorsContainer"]/ul[@id="itemColors"]/li[@title]/@title').extract()
+        ]
+        if colors:
+            metadata['color'] = colors
+
+
+        description = '\r'.join(
+            self.reformat(val)
+            for val in sel.xpath('//div[@id="description_pane"]/div[@class="itemDesc"]//text()').extract()
+        )
+        description = self.reformat(description)
+        if description:
+            metadata['description'] = description
+
+
+        detail = '\r'.join(
+            self.reformat(val)
+            for val in sel.xpath('//div[@id="description_pane"]/div[@class="details"]//text()').extract()
+        )
+        detail = self.reformat(detail)
+        if detail:
+            metadata['details'] = detail
+
+
+        # 下边是取的图片url
+        image_urls = []
+
+        # 另一些颜色的url，与当前node的url区别在于一个叫data-cod10的东西
+        # 根据从颜色标签中取的的data-cod10，生成另一种颜色的图片url
+        color_codes = [
+            self.reformat(val)
+            for val in sel.xpath('//div[@class="itemColorsContainer"]/ul[@id="itemColors"]/li[@data-cod10]/@data-cod10').extract()
+        ]
+
+        # 这里只取到了当前显示颜色的node，
+        # 这里经过测试，即使当前颜色没有一些角度的图片，这里也能取到url，页面上会有1px的一些占位
+        # 例如：http://www.alexanderwang.cn/cn/%E7%9F%AD%E6%AC%BE%E8%BF%9E%E8%A1%A3%E8%A3%99_cod34283023ab.html
+        image_nodes = sel.xpath('//div[@class="itemImages"]/ul[@id="imageList"]/li/img[@src]')
+        for node in image_nodes:
+            origin_src = node.xpath('./@src').extract()[0]
+            origin_src = self.process_href(origin_src, response.url)
+
+            # 其他颜色的图片src
+            all_color_srcs = [
+                re.sub(r'/[0-9A-Za-z]+_', str.format('/{0}_', val), origin_src)
+                for val in color_codes
+            ]
+
+            # 不同尺寸的图片
+            image_urls += [
+                re.sub(r'_\d+_', str.format('_{0}_', val), src)
+                for val in xrange(12, 17)
+                for src in all_color_srcs
+            ]
+
+        item = ProductItem()
+        item['url'] = metadata['url']
+        item['model'] = metadata['model']
+        if image_urls:
+            item['image_urls'] = image_urls
+        item['metadata'] = metadata
+
+        yield item
+
