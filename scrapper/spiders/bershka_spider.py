8c6252ddef1bded9ac04a9f0863a7405ba7385db
dhcdht <dhcdht@qq.com>
2013-12-19 14:29:54 +0800

    temp

--- scrapper/spiders/bershka_spider.py
+++ scrapper/spiders/bershka_spider.py
@@ -7,6 +7,11 @@ from scrapper.items import ProductItem
 from scrapy.http import Request
 from scrapy.selector import Selector
 
+import copy
+import common
+import re
+import json
+
 class BershkaSpider(MFashionSpider):
 
     spider_data = {
@@ -27,3 +32,157 @@ class BershkaSpider(MFashionSpider):
 
         metadata = response.meta['userdata']
         sel = Selector(response)
+
+        nav_nodes = sel.xpath('//div[@id="main_menu_menu"]/ul/li')
+        for node in nav_nodes:
+            m = copy.deepcopy(metadata)
+
+            tag_text = ''.join(
+                self.reformat(val)
+                for val in node.xpath('.//text()').extract()
+            )
+            tag_text = self.reformat(tag_text)
+            tag_name = tag_text.lower()
+
+            if tag_text and tag_name:
+                m['tags_mapping']['category-0'] = [
+                    {'name': tag_name, 'title': tag_text,},
+                ]
+
+                gender = common.guess_gender(tag_name)
+                if gender:
+                    m['gender'] = [gender]
+
+            href = node.xpath('./a[@href]/@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_left_nav,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+    def parse_left_nav(self, response):
+        """
+        这个左侧当前进入的类别会被展开
+        展开的类别下属虽然还可以再展开，但是只有所有和bershka两个选项，没用
+        当前类别右侧的系列，会根据当前类别变，应该算是当前类别的下属
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        # 当前类别的下属第二级类别
+        left_nav_nodes = sel.xpath('//div[@id="col_colizq"]/div[@id="col_list"]/ul/li[child::a[text()]]')
+        for node in left_nav_nodes:
+            m = copy.deepcopy(metadata)
+
+            tag_text = node.xpath('./a[text()]/text()').extract()[0]
+            tag_text = self.reformat(tag_text)
+            tag_name = tag_text.lower()
+
+            if tag_text and tag_name:
+                m['tags_mapping']['category-1'] = [
+                    {'name': tag_name, 'title': tag_text,},
+                ]
+
+                gender = common.guess_gender(tag_name)
+                if gender:
+                    m['gender'] = [gender]
+
+            href = node.xpath('./a[@href]/@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product_list,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+        # 当前类别的下属系列页面链接
+        collection_node = sel.xpath('//div[@id="col_colizq"]/div[@id="subhome_pest1"]/a[@href]')
+        if collection_node:
+            href = collection_node.xpath('./@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_left_nav_collection,
+                          errback=self.onerr,
+                          meta={'userdata': metadata})
+
+        # 解析当前页面的单品列表
+        for val in self.parse_product_list(response):
+            yield val
+
+    def parse_left_nav_collection(self, response):
+        """
+        这里解析左边当前类别右侧的系列，看起来是当前类别的下属
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        sub_nodes = sel.xpath('//div[@id="col_colizq"]/div[@id="col_list"]/ul/li[child::a[text()]]')
+        for sub_node in sub_nodes:
+            m = copy.deepcopy(metadata)
+
+            tag_text = sub_node.xpath('./a/text()').extract()[0]
+            tag_text = self.reformat(tag_text)
+            tag_name = tag_text.lower()
+
+            if tag_text and tag_name:
+                m['tags_mapping']['category-1'] = [
+                    {'name': tag_name, 'title': tag_text,},
+                ]
+
+                gender = common.guess_gender(tag_name)
+                if gender:
+                    m['gender'] = [gender]
+
+            href = sub_node.xpath('./a/@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product_list,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+    def parse_product_list(self, response):
+        """
+        解析单品列表
+
+        注意：这里所有的单品信息都在js的一个json里边，直接解析response，啥都得不到
+        """
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        # 这里取它的json内容loads的话，总是有错误，所以用这种正则表达式的办法
+        url_list = re.findall(r'url: "(.+)/_COLOR_"', response.body)
+        for url in url_list:
+            m = copy.deepcopy(metadata)
+
+            href = self.process_href(url, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product,
+                          errback=self.onerr,
+                          meta={'userdata': m},
+                          dont_filter=True)
+
+    def parse_product(self, response):
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        metadata['url'] = response.url
+
+        model = None
+        model_node = sel.xpath('//div[@id="info1"]/div[@class="padding15"]/table//td[not(child::*)][2]')
+        if model_node:
+            model = model_node.xpath('./text()').extract()[0]
+            model = self.reformat(model)
+
+        if model:
+            metadata['model'] = model
+        else:
+            return
+
