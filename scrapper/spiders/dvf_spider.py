a3b99e2bc4c393df8f72aa8f3e3375c1cf8c9b5e
dhcdht <dhcdht@qq.com>
2014-01-20 17:16:55 +0800

    添加dvf的爬虫，美国

---
+++ scrapper/spiders/dvf_spider.py
@@ -0,0 +1,231 @@
+# coding=utf-8
+
+__author__ = 'Ryan'
+
+from scrapper.spiders.mfashion_spider import MFashionSpider
+from scrapper.items import ProductItem
+from scrapy.http import Request
+from scrapy.selector import Selector
+
+import common
+import copy
+import re
+
+class DVFSpider(MFashionSpider):
+
+    spider_data = {
+        'brand_id': 10617,
+        'home_urls': {
+            'us': 'http://www.dvf.com/',
+        },
+    }
+
+    @classmethod
+    def get_supported_regions(cls):
+        return cls.spider_data['home_urls'].keys()
+
+    def __init__(self, region):
+        super(DVFSpider, self).__init__('dvf', region)
+
+    def parse(self, response):
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        nav_nodes = sel.xpath('//div[@id="container"]/div[@id="header"]/div[@class="cont-main-menu"]/div[@class="inner-menu"]/ul/li[child::a[@href][text()]]')
+        for node in nav_nodes:
+            tag_text = node.xpath('./a/text()').extract()[0]
+            tag_text = self.reformat(tag_text)
+            tag_name = tag_text.lower()
+
+            if tag_text and tag_name:
+                m = copy.deepcopy(metadata)
+
+                m['tags_mapping']['category-0'] = [
+                    {'name': tag_name, 'title': tag_text,},
+                ]
+
+                gender = common.guess_gender(tag_name)
+                if gender:
+                    m['gender'] = [gender]
+
+                sub_nodes = node.xpath('./ul/li[child::a[@href][text()]]')
+                for sub_node in sub_nodes:
+                    tag_text = sub_node.xpath('./a/text()').extract()[0]
+                    tag_text = self.reformat(tag_text)
+                    tag_name = tag_text.lower()
+
+                    if tag_text and tag_name:
+                        mc = copy.deepcopy(m)
+
+                        mc['tags_mapping']['category-1'] = [
+                            {'name': tag_name, 'title': tag_text,},
+                        ]
+
+                        gender = common.guess_gender(tag_name)
+                        if gender:
+                            mc['gender'] = [gender]
+
+                        href = sub_node.xpath('./a/@href').extract()[0]
+                        href = self.process_href(href, response.url)
+
+                        yield Request(url=href,
+                                      callback=self.parse_product_list,
+                                      errback=self.onerr,
+                                      meta={'userdata': mc})
+
+    def parse_product_list(self, response):
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+        product_nodes = sel.xpath('//div[@id="main"]//div[@id="primary"]/div[@class="search-result-content"]/ul/li')
+        for node in product_nodes:
+            m = copy.deepcopy(metadata)
+
+            href = node.xpath('.//a/@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product,
+                          errback=self.onerr,
+                          meta={'userdata': m},
+                          dont_filter=True)
+
+        # 页面下拉到底部会自动加载更多，需要模拟请求，解析返回的json
+        # 测试发现，在原有url后边添加 ?start=20 也可以取到第二页内容
+        # 如果当前页有内容，再考虑请求下一页，每页有20个
+        if product_nodes:
+            # 取的当前页数
+            current_start = 0
+            mt = re.search(r'start=(\d+)', response.url)
+            if mt:
+                current_page = (int)(mt.group(1))
+
+            next_start = current_start + 20
+            # 拼下一页的url
+            if mt:
+                next_url = re.sub(r'start=\d+', str.format('start={0}', next_start), response.url)
+            else:
+                next_url = str.format('{0}?start={1}', response.url, next_start)
+
+            # 请求下一页
+            yield Request(url=next_url,
+                          callback=self.parse_product_list,
+                          errback=self.onerr,
+                          meta={'userdata': metadata})
+
+    def parse_product(self, response):
+
+        metadata = response.meta['userdata']
+        sel = Selector(response)
+
+
+        other_product_nodes = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-variations"]/ul/li/div/ul[@class="swatches Color"]/li/a[@href]')
+        for node in other_product_nodes:
+            m = copy.deepcopy(metadata)
+
+            href = node.xpath('./@href').extract()[0]
+            href = self.process_href(href, response.url)
+
+            yield Request(url=href,
+                          callback=self.parse_product,
+                          errback=self.onerr,
+                          meta={'userdata': m})
+
+
+        metadata['url'] = response.url
+
+
+        model = None
+        model_node = sel.xpath('//div[@id="container"][@data-pid]')
+        if model_node:
+            model = model_node.xpath('./@data-pid').extract()[0]
+            model = self.reformat(model)
+
+        if model:
+            metadata['model'] = model
+        else:
+            return
+
+
+        name = None
+        name_node = sel.xpath('//div[@id="content"]//div[@id="product-content"]//h1[@class="product-name"][text()]')
+        if name_node:
+            name = name_node.xpath('./text()').extract()[0]
+            name = self.reformat(name)
+
+        if name:
+            metadata['name'] = name
+
+
+        colors = []
+        color_nodes = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-variations"]/ul/li/div/ul[@class="swatches Color"]/li/a[@data-colorname]')
+        for color_node in color_nodes:
+            color_name = color_node.xpath('./@data-colorname').extract()[0]
+            colors += [color_name]
+
+        if colors:
+            metadata['color'] = colors
+
+
+        old_price = None
+        new_price = None
+        discount_node = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-price"]/span[@class="price-standard"][text()]')
+        if discount_node:
+            old_price = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-price"]/span[@class="price-standard"][text()]/text()').extract()[0]
+            old_price = self.reformat(old_price)
+
+            new_price = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-price"]/span[@class="price-sales"][text()]/text()').extract()[0]
+            new_price = self.reformat(new_price)
+        else:
+            old_price = sel.xpath('//div[@id="content"]//div[@id="product-content"]//div[@class="product-price"]/span[@class="price-sales"][text()]/text()').extract()[0]
+            old_price = self.reformat(old_price)
+
+        if old_price:
+            metadata['price'] = old_price
+        if new_price:
+            metadata['price_discount'] = new_price
+
+
+        description = None
+        description_node = sel.xpath('//div[@id="content"]//div[@itemprop="description"][text()]')
+        if description_node:
+            description = description_node.xpath('./text()').extract()[0]
+            description = self.reformat(description)
+
+        if description:
+            metadata['description'] = description
+
+
+        detail = None
+        detail_node = sel.xpath('//div[@id="content"]//p[@class="tab-fabric-description"][text()]')
+        if detail_node:
+            detail = detail_node.xpath('./text()').extract()[0]
+            detail = self.reformat(detail)
+
+        if detail:
+            metadata['details'] = detail
+
+
+        image_urls = []
+        origin_image_node = sel.xpath('//div[@id="content"]//div[@id="pdp-pinterest-container"]/img[@src]')
+        if origin_image_node:
+            origin_image_url = origin_image_node.xpath('./@src').extract()[0]
+            origin_image_url = self.process_href(origin_image_url, response.url)
+            origin_image_url = re.sub(ur'\?.*$', ur'_A1?$Demandware%20Large%20Rectangle$', origin_image_url)
+            if origin_image_url:
+                image_urls = [
+                    re.sub(ur'_A\d\?', str.format(r'_A{0}?', val), origin_image_url)
+                    for val in xrange(1, 5)
+                ]
+
+
+        item = ProductItem()
+        item['url'] = metadata['url']
+        item['model'] = metadata['model']
+        if image_urls:
+            item['image_urls'] = image_urls
+        item['metadata'] = metadata
+
+        yield item
