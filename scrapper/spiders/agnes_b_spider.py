84e173f683066ae665182541af921a8cf8eff43c
dhcdht <dhcdht@qq.com>
2014-02-10 16:23:20 +0800

    agnes_b的爬虫，添加try except，提取fetch_[name, description, detail]方法

--- scrapper/spiders/agnes_b_spider.py
+++ scrapper/spiders/agnes_b_spider.py
@@ -42,9 +42,12 @@ class AgnesBSpider(MFashionSpider):
         for node in nav_nodes:
             m = copy.deepcopy(metadata)
 
-            tag_text = node.xpath('./text()').extract()[0]
-            tag_text = self.reformat(tag_text)
-            tag_name = tag_text.lower()
+            try:
+                tag_text = node.xpath('./text()').extract()[0]
+                tag_text = self.reformat(tag_text)
+                tag_name = tag_text.lower()
+            except(TypeError, IndexError):
+                continue
 
             if tag_text and tag_name:
                 m['tags_mapping']['category-0'] = [
@@ -55,8 +58,11 @@ class AgnesBSpider(MFashionSpider):
                 if gender:
                     m['gender'] = [gender]
 
-                href = node.xpath('./@href').extract()[0]
-                href = self.process_href(href, response.url)
+                try:
+                    href = node.xpath('./@href').extract()[0]
+                    href = self.process_href(href, response.url)
+                except(TypeError, IndexError):
+                    continue
 
                 yield Request(url=href,
                               callback=self.parse_filter1,
@@ -76,9 +82,12 @@ class AgnesBSpider(MFashionSpider):
         for sub_node in sub_nodes:
             m = copy.deepcopy(metadata)
 
-            tag_text = sub_node.xpath('./text()').extract()[0]
-            tag_text = self.reformat(tag_text)
-            tag_name = tag_text.lower()
+            try:
+                tag_text = sub_node.xpath('./text()').extract()[0]
+                tag_text = self.reformat(tag_text)
+                tag_name = tag_text.lower()
+            except(TypeError, IndexError):
+                continue
 
             if tag_text and tag_name:
                 m['tags_mapping']['category-1'] = [
@@ -89,8 +98,11 @@ class AgnesBSpider(MFashionSpider):
                 if gender:
                     m['gender'] = [gender]
 
-                href = sub_node.xpath('./@href').extract()[0]
-                href = self.process_href(href, response.url)
+                try:
+                    href = sub_node.xpath('./@href').extract()[0]
+                    href = self.process_href(href, response.url)
+                except(TypeError, IndexError):
+                    continue
 
                 yield Request(url=href,
                               callback=self.parse_filter2,
@@ -113,9 +125,12 @@ class AgnesBSpider(MFashionSpider):
         for sub_node in sub_nodes:
             m = copy.deepcopy(metadata)
 
-            tag_text = sub_node.xpath('./text()').extract()[0]
-            tag_text = self.reformat(tag_text)
-            tag_name = tag_text.lower()
+            try:
+                tag_text = sub_node.xpath('./text()').extract()[0]
+                tag_text = self.reformat(tag_text)
+                tag_name = tag_text.lower()
+            except(TypeError, IndexError):
+                continue
 
             if tag_text and tag_name:
                 m['tags_mapping']['category-2'] = [
@@ -126,8 +141,11 @@ class AgnesBSpider(MFashionSpider):
                 if gender:
                     m['gender'] = [gender]
 
-                href = sub_node.xpath('./@href').extract()[0]
-                href = self.process_href(href, response.url)
+                try:
+                    href = sub_node.xpath('./@href').extract()[0]
+                    href = self.process_href(href, response.url)
+                except(TypeError, IndexError):
+                    continue
 
                 yield Request(url=href,
                               callback=self.parse_product_list,
@@ -147,12 +165,15 @@ class AgnesBSpider(MFashionSpider):
         for node in product_nodes:
             m = copy.deepcopy(metadata)
 
-            name_node = node.xpath('./p/a[text()]')
-            if name_node:
-                name = name_node.xpath('./text()').extract()[0]
-                name = self.reformat(name)
-                if name:
-                    metadata['name'] = name
+            try:
+                name_node = node.xpath('./p/a[text()]')
+                if name_node:
+                    name = name_node.xpath('./text()').extract()[0]
+                    name = self.reformat(name)
+                    if name:
+                        metadata['name'] = name
+            except(TypeError, IndexError):
+                continue
 
             # old_price_node = node.xpath('./span[@class="prix_old"][text()]')
             # if old_price_node:
@@ -175,12 +196,15 @@ class AgnesBSpider(MFashionSpider):
             #         if price:
             #             metadata['price'] = price
 
-            colors = [
-                self.reformat(val).lower()
-                for val in node.xpath('/p/a/img[@title]/@title').extract()
-            ]
-            if colors:
-                metadata['color'] = colors
+            try:
+                colors = [
+                    self.reformat(val).lower()
+                    for val in node.xpath('/p/a/img[@title]/@title').extract()
+                ]
+                if colors:
+                    metadata['color'] = colors
+            except(TypeError, IndexError):
+                continue
 
             # 这里随便取一个a标签就行
             href = node.xpath('./a[@href]/@href').extract()[0]
@@ -200,23 +224,32 @@ class AgnesBSpider(MFashionSpider):
         old_price_node = sel.xpath(
             '//div[@class="description_bas"]/div[@id="test"]/div[@class="tarif_old"]/span[text()]')
         if old_price_node:
-            old_price = old_price_node.xpath('./text()').extract()[0]
-            old_price = cls.reformat(old_price)
-            if old_price:
-                ret['price'] = old_price
+            try:
+                old_price = old_price_node.xpath('./text()').extract()[0]
+                old_price = cls.reformat(old_price)
+                if old_price:
+                    ret['price'] = old_price
+            except(TypeError, IndexError):
+                pass
 
             new_price_node = sel.xpath(
                 '//div[@class="description_bas"]/div[@id="test"]/div[contains(@class, "discount")][text()]')
             if new_price_node:
-                new_price = new_price_node.xpath('./text()').extract()[0]
-                new_price = cls.reformat(new_price)
-                if new_price:
-                    ret['price_discount'] = new_price
+                try:
+                    new_price = new_price_node.xpath('./text()').extract()[0]
+                    new_price = cls.reformat(new_price)
+                    if new_price:
+                        ret['price_discount'] = new_price
+                except(TypeError, IndexError):
+                    pass
         else:
-            price = sel.xpath('//div[@class="description_bas"]/div[@id="test"]/div[text()]/text()').extract()[0]
-            price = cls.reformat(price)
-            if price:
-                ret['price'] = price
+            try:
+                price = sel.xpath('//div[@class="description_bas"]/div[@id="test"]/div[text()]/text()').extract()[0]
+                price = cls.reformat(price)
+                if price:
+                    ret['price'] = price
+            except(TypeError, IndexError):
+                pass
 
         return ret
 
@@ -227,15 +260,20 @@ class AgnesBSpider(MFashionSpider):
     @classmethod
     def fetch_model(cls, response):
         sel = Selector(response)
+
         # 货号在点开+details中的reference后边
         model = None
         model_node = sel.xpath('//div[@id="details_popup"]/div[not(@class)][text()]')
         if model_node:
-            model_text = model_node.xpath('./text()').extract()[0]
-            model_text = cls.reformat(model_text)
-            mt = re.search(r'\b(\w+)\b$', model_text)
-            if mt:
-                model = mt.group(1)
+            try:
+                model_text = model_node.xpath('./text()').extract()[0]
+                model_text = cls.reformat(model_text)
+                mt = re.search(r'\b(\w+)\b$', model_text)
+                if mt:
+                    model = mt.group(1)
+            except(TypeError, IndexError):
+                pass
+
         return model
 
     def parse_product(self, response):
@@ -248,8 +286,11 @@ class AgnesBSpider(MFashionSpider):
         for node in color_nodes:
             m = copy.deepcopy(metadata)
 
-            href = node.xpath('./@href').extract()[0]
-            href = self.process_href(href, response.url)
+            try:
+                href = node.xpath('./@href').extract()[0]
+                href = self.process_href(href, response.url)
+            except(TypeError, IndexError):
+                continue
 
             yield Request(url=href,
                           callback=self.parse_product,
@@ -265,10 +306,12 @@ class AgnesBSpider(MFashionSpider):
             return
 
         if not metadata.get('name'):
-            name = sel.xpath('//div[@id="infos_produit"]/div/div/p[@class="titre"][text()]/text()').extract()[0]
-            name = self.reformat(name)
-            if name:
-                metadata['name'] = name
+            try:
+                name = self.fetch_name(response)
+                if name:
+                    metadata['name'] = name
+            except(TypeError, IndexError):
+                pass
 
         if not metadata.get('price'):
             ret = self.fetch_price(response)
@@ -278,44 +321,36 @@ class AgnesBSpider(MFashionSpider):
                 metadata['price_discount'] = ret['price_discount']
 
         if not metadata.get('color'):
-            colors = [
-                self.reformat(val).lower()
-                for val in sel.xpath('//div[@class="carre_couleur"]/a/img[@title]/@title').extract()
-            ]
-            if colors:
-                metadata['color'] = colors
+            try:
+                colors = [
+                    self.reformat(val).lower()
+                    for val in sel.xpath('//div[@class="carre_couleur"]/a/img[@title]/@title').extract()
+                ]
+                if colors:
+                    metadata['color'] = colors
+            except(TypeError, IndexError):
+                pass
 
-        # 这里你点开那个+details，才能看到完整的description
-        # 我把上面作为description，下面作为detail
-        description_node = sel.xpath('//div[@id="details_popup"]/div[@class="gauche"]')
-        if description_node:
-            description = ''.join(
-                self.reformat(val)
-                for val in description_node.xpath('./text()').extract()
-            )
-            description = self.reformat(description)
-            if description:
-                metadata['description'] = description
+        description = self.fetch_description(response)
+        if description:
+            metadata['description'] = description
 
-            detail_node = description_node.xpath('./div/p[text()]')
-            if detail_node:
-                detail = '\r'.join(
-                    self.reformat(val)
-                    for val in detail_node.xpath('./text()').extract()
-                )
-                detail = self.reformat(detail)
-                if detail:
-                    metadata['details'] = detail
+        detail = self.fetch_details(response)
+        if detail:
+            metadata['details'] = detail
 
         # 这里，1500的图片应该都有
         # 我觉得他有更大的图，虽然更大的值返回404，我觉得是对加载大图片有限制它
         image_urls = None
         image_nodes = sel.xpath('//div[@id="ProductDetailThumbList"]//a/img[@src]')
         if image_nodes:
-            image_urls = [
-                re.sub(r'_80', r'_1500', self.process_href(val, response.url))
-                for val in image_nodes.xpath('./@src').extract()
-            ]
+            try:
+                image_urls = [
+                    re.sub(r'_80', r'_1500', self.process_href(val, response.url))
+                    for val in image_nodes.xpath('./@src').extract()
+                ]
+            except(TypeError, IndexError):
+                pass
 
         item = ProductItem()
         item['url'] = metadata['url']
@@ -326,3 +361,57 @@ class AgnesBSpider(MFashionSpider):
 
         yield item
 
+    @classmethod
+    def fetch_name(cls, response):
+        sel = Selector(response)
+
+        name = None
+        try:
+            name = sel.xpath('//div[@id="infos_produit"]/div/div/p[@class="titre"][text()]/text()').extract()[0]
+            name = cls.reformat(name)
+        except(TypeError, IndexError):
+            pass
+
+        return name
+
+    @classmethod
+    def fetch_description(cls, response):
+        sel = Selector(response)
+
+        # 这里你点开那个+details，才能看到完整的description
+        # 我把上面作为description，下面作为detail
+        description = None
+        description_node = sel.xpath('//div[@id="details_popup"]/div[@class="gauche"]')
+        if description_node:
+            try:
+                description = ''.join(
+                    cls.reformat(val)
+                    for val in description_node.xpath('./text()').extract()
+                )
+                description = cls.reformat(description)
+            except(TypeError, IndexError):
+                pass
+
+        return description
+
+    @classmethod
+    def fetch_details(cls, response):
+        sel = Selector(response)
+
+        # 这里你点开那个+details，才能看到完整的description
+        # 我把上面作为description，下面作为detail
+        detail = None
+        description_node = sel.xpath('//div[@id="details_popup"]/div[@class="gauche"]')
+        if description_node:
+            detail_node = description_node.xpath('./div/p[text()]')
+            if detail_node:
+                try:
+                    detail = '\r'.join(
+                        cls.reformat(val)
+                        for val in detail_node.xpath('./text()').extract()
+                    )
+                    detail = cls.reformat(detail)
+                except(TypeError, IndexError):
+                    pass
+
+        return detail
