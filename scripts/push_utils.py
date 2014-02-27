6ebbe25cea3bef4423439feef115d98e3558e682
Haizi Zheng <haizi.zh@gmail.com>
2014-02-27 13:06:55 +0800

    FIX BUG: IMPROVED PUSHING UTILITIES.
    1. 获得新增单品时，添加单品优先名称的信息。
    2. 获得价格变化的单品时，添加单品优先名称的信息。
    3. 只关注20个优先国家的单品价格变化趋势。

--- scripts/push_utils.py
+++ scripts/push_utils.py
@@ -2,6 +2,7 @@
 import datetime
 from core import MySqlDb
 import global_settings as gs
+from utils.utils import unicodify
 
 __author__ = 'Zephyre'
 
@@ -52,84 +53,113 @@ def price_changed(brand_list=None, start=None, end=None):
         else:
             return err_no
 
-    db = MySqlDb()
-    db.conn(gs.DB_SPEC)
+    # 主要国家列表。只监控这些国家的单品的价格变化过程。
+    main_countries = [tmp[0] for tmp in filter(lambda val: val[1]['weight'] < 999999, gs.region_info().items())]
+    with MySqlDb(getattr(gs, 'DB_SPEC')) as db:
+        if not brand_list:
+            rs = db.query_match(['brand_id'], 'products', distinct=True)
+            brand_list = [int(val[0]) for val in rs.fetch_row(maxrows=0)]
 
-    # if not brand_list:
-    #     rs = db.query_match(['brand_id'], 'products', distinct=True)
-    #     brand_list = [int(val[0]) for val in rs.fetch_row(maxrows=0)]
+        if not (start and end):
+            # 获得默认的时间区间
+            start = (datetime.datetime.now() - datetime.timedelta(1)).date()
+            end = datetime.datetime.now().date()
 
-    if not (start and end):
-        # 获得默认的时间区间
-        start = (datetime.datetime.now() - datetime.timedelta(1)).date()
-        end = datetime.datetime.now().date()
+        results = {'warnings': [], 'price_up': {}, 'discount_up': {}, 'price_down': {}, 'discount_down': {}}
+        for brand in brand_list:
+            pid_list = db.query(str.format('''SELECT p1.model,p1.idproducts,p1.region,p1.fingerprint FROM products AS p1
+            JOIN products_price_history AS p2 ON p1.idproducts=p2.idproducts
+            WHERE p1.offline=0 AND p2.price IS NOT NULL AND brand_id={0} AND p1.region IN ({1}) AND p2.date BETWEEN {2} AND {3}''',
+                                           brand, ','.join(str.format('"{0}"', tmp) for tmp in main_countries),
+                                           *map(lambda val: val.strftime('"%Y-%m-%d %H:%M:%S"'),
+                                                (start, end)))).fetch_row(maxrows=0)
+            if not pid_list:
+                continue
 
-    results = {'warnings': [], 'price_up': {}, 'discount_up': {}, 'price_down': {}, 'discount_down': {}}
-    processed = set({})
+            tmp = db.query(str.format('''
+            SELECT p1.idproducts,p1.model,p1.region,p1.fingerprint,p2.price,p2.price_discount,p2.currency,p2.date FROM products AS p1
+            JOIN products_price_history AS p2 ON p1.idproducts=p2.idproducts
+            WHERE p1.idproducts IN ({0}) ORDER BY p2.date DESC''', ','.join(tmp[1] for tmp in pid_list))).fetch_row(
+                maxrows=0)
+            # 按照pid归并
+            rs = {}
+            pid_set = set([val[0] for val in tmp])
+            for pid, model, region, fp, price, discount, currency, date in tmp:
+                if pid not in pid_set:
+                    continue
+                if int(pid) in rs and len(rs[int(pid)]) >= 2:
+                    # 最近两条数据已满，跳过该pid
+                    pid_set.remove(pid)
+                    continue
 
-    brand_cond = str.format('p1.brand_id IN ({0})', ','.join(str(tmp) for tmp in brand_list)) if brand_list else 1
-    rs_pid = db.query(str.format('''SELECT p1.brand_id,p1.model,p1.idproducts,p1.region,p1.fingerprint FROM products AS p1
-    JOIN products_price_history AS p2 ON p1.idproducts=p2.idproducts
-    WHERE p1.offline=0 AND p2.price IS NOT NULL AND {0} AND p2.date BETWEEN {1} AND {2}''',
-                                 brand_cond,
-                                 *map(lambda val: val.strftime('"%Y-%m-%d %H:%M:%S"'), (start, end)))).fetch_row(
-        maxrows=0)
-    for brand, model, pid, region, fingerprint in rs_pid:
-        pid = int(pid)
-        brand = int(brand)
-        # 在某个特定的时间区间内，可能存在同一个pid的多条记录。每个pid我们只需要处理一次即可。
-        if pid in processed:
-            continue
-        else:
-            processed.add(pid)
-        # 查找该pid的价格记录，并排序
-        rs = sorted(
-            db.query_match(['*'], 'products_price_history', {'idproducts': pid}).fetch_row(maxrows=0, how=1),
-            key=lambda val: datetime.datetime.strptime(val['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
-        # 查找最近两次的价格并比较
-        if len(rs) < 2:
-            continue
+                pid = int(pid)
+                if pid not in rs:
+                    rs[pid] = []
+                rs[pid].append([model, region, fp, price, discount, currency, date])
 
-        def func(idx):
-            rate = gs.currency_info()[rs[idx]['currency']]
-            return (float(rs[idx]['price']) * rate if rs[idx]['price'] else None,
-                    float(rs[idx]['price_discount']) * rate if rs[idx]['price_discount'] else None)
+            for pid, price_history in rs.items():
+                if len(price_history) < 2:
+                    continue
 
-        price1, discount1 = func(0)
-        price2, discount2 = func(1)
+                def func(idx):
+                    rate = gs.currency_info()[price_history[idx][-2]]
+                    return (float(price_history[idx][-4]) * rate if price_history[idx][-4] else None,
+                            float(price_history[idx][-3]) * rate if price_history[idx][-3] else None)
 
-        # 是否可能有错误？
-        ret = price_check((price2, discount2), (price1, discount1))
-        if ret != 0:
-            results['warnings'].append({'idproducts': pid, 'model': model, 'msg': ret[1]})
-            continue
+                price1, discount1 = func(0)
+                price2, discount2 = func(1)
 
-        if price1 and price2 and price1 < price2:
-            key = 'price_down'
-        elif price1 and price2 and price1 > price2:
-            key = 'price_up'
-        elif discount1 and discount2 and discount1 < discount2:
-            key = 'discount_down'
-        elif discount1 and discount2 and discount1 > discount2:
-            key = 'discount_up'
-        else:
-            key = None
+                # 是否可能有错误？
+                ret = price_check((price2, discount2), (price1, discount1))
+                if ret != 0:
+                    results['warnings'].append({'idproducts': pid, 'model': model, 'msg': ret[1]})
+                    continue
 
-        if key:
-            if fingerprint not in results[key]:
-                results[key][fingerprint] = []
-            results[key][fingerprint].append({'idproducts': pid, 'model': model, 'region': region, 'brand_id': brand,
-                                              'fingerprint': fingerprint,
-                                              'old_price': {'price': rs[1]['price'],
-                                                            'price_discount': rs[1]['price_discount'],
-                                                            'currency': rs[1]['currency']},
-                                              'new_price': {'price': rs[0]['price'],
-                                                            'price_discount': rs[0]['price_discount'],
-                                                            'currency': rs[0]['currency']}
-            })
+                if price1 and price2 and price1 < price2:
+                    key = 'price_down'
+                elif price1 and price2 and price1 > price2:
+                    key = 'price_up'
+                elif discount1 and discount2 and discount1 < discount2:
+                    key = 'discount_down'
+                elif not discount2 and discount1:
+                    key = 'discount_down'
+                elif discount1 and discount2 and discount1 > discount2:
+                    key = 'discount_up'
+                elif not discount1 and discount2:
+                    key = 'discount_up'
+                else:
+                    key = None
 
-    db.close()
-    return results
+                if key:
+                    if brand not in results[key]:
+                        results[key][brand] = {}
+
+                    fp = price_history[0][2]
+                    if fp not in results[key][brand]:
+                        name = unicodify(sorted(
+                            db.query_match(['name', 'region'], 'products', {'fingerprint': fp}).fetch_row(
+                                maxrows=0, how=1),
+                            key=lambda val: gs.region_info()[val['region']]['weight'])[0]['name'])
+                        results[key][brand][fp] = {'model': model, 'brand_id': brand, 'fingerprint': fp,
+                                                   'products': [], 'name': name}
+
+                    # 获得单品的优先名称
+                    region = price_history[0][1]
+                    price_new = float(price_history[0][3]) if price_history[0][3] else None
+                    price_old = float(price_history[1][3]) if price_history[1][3] else None
+                    discount_new = float(price_history[0][4]) if price_history[0][4] else None
+                    discount_old = float(price_history[1][4]) if price_history[1][4] else None
+                    currency_new = price_history[0][5]
+                    currency_old = price_history[1][5]
+                    results[key][brand][fp]['products'].append(
+                        {'idproducts': int(pid), 'region': region,
+                         'old_price': {'price': price_old, 'price_discount': discount_old,
+                                       'currency': currency_old},
+                         'new_price': {'price': price_new, 'price_discount': discount_new,
+                                       'currency': currency_new}
+                        })
+
+        return results
 
 
 def newly_fetched(brand_list=None, start=None, end=None):
@@ -140,31 +170,38 @@ def newly_fetched(brand_list=None, start=None, end=None):
     @param start: datetime.date或datetime.datetime对象
     @param end:
     """
-    db = MySqlDb()
-    db.conn(gs.DB_SPEC)
+    with MySqlDb(getattr(gs, 'DB_SPEC')) as db:
+        if not brand_list:
+            rs = db.query_match(['brand_id'], 'products', distinct=True)
+            brand_list = [int(val[0]) for val in rs.fetch_row(maxrows=0)]
 
-    if not brand_list:
-        rs = db.query_match(['brand_id'], 'products', distinct=True)
-        brand_list = [int(val[0]) for val in rs.fetch_row(maxrows=0)]
+        if not (start and end):
+            # 获得默认的时间区间
+            start = (datetime.datetime.now() - datetime.timedelta(1)).date()
+            end = datetime.datetime.now().date()
 
-    if not (start and end):
-        # 获得默认的时间区间
-        start = (datetime.datetime.now() - datetime.timedelta(1)).date()
-        end = datetime.datetime.now().date()
+        results = {}
+        processed = set({})
+        for brand in brand_list:
+            records = db.query_match(['*'], 'products', {'brand_id': brand},
+                                     [str.format('fetch_time BETWEEN {0} AND {1}',
+                                                 *map(lambda val: val.strftime('"%Y-%m-%d %H:%M:%S"'),
+                                                      (start, end))), 'offline=0']).fetch_row(maxrows=0, how=1)
+            for r in records:
+                fp = r['fingerprint']
+                if fp not in processed:
+                    if brand not in results:
+                        results[brand] = []
 
-    results = {}
-    processed = set({})
-    for brand in brand_list:
-        records = db.query_match(['*'], 'products', {'brand_id': brand},
-                                 [str.format('fetch_time BETWEEN {0} AND {1}',
-                                             *map(lambda val: val.strftime('"%Y-%m-%d %H:%M:%S"'),
-                                                  (start, end))), 'offline=0']).fetch_row(maxrows=0, how=1)
-        for r in records:
-            fp = r['fingerprint']
-            if fp not in processed:
-                if brand not in results:
-                    results[brand] = []
-                results[brand].append({'model': r['model'], 'fingerprint': fp})
-                processed.add(fp)
-    db.close()
-    return results
+                    # 按照fingerprint进行归并，并按照国家的权重进行排序
+                    tmp = sorted(filter(lambda val: val['fingerprint'] == fp, records),
+                                 key=lambda val: gs.region_info()[val['region']]['weight'])
+                    results[brand].append(
+                        {'model': tmp[0]['model'], 'fingerprint': fp, 'name': unicodify(tmp[0]['name'])})
+                    processed.add(fp)
+        return results
+
+
+if __name__ == '__main__':
+    rec = price_changed()
+    pass
