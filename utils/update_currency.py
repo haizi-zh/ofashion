b89737a749d7ce82293d14bb01ace473c8509f09
Haizi Zheng <haizi.zh@gmail.com>
2014-03-17 12:12:38 +0800

    MINOR AMENDMENT

--- utils/update_currency.py
+++ utils/update_currency.py
@@ -15,25 +15,24 @@ class CurrencyUpdate(object):
         更新货币的汇率信息
         @param param_dict:
         """
-        db = RoseVisionDb()
-        db.conn(gs.DB_SPEC)
-        rs = db.query_match(['iso_code', 'currency'], 'region_info').fetch_row(maxrows=0)
-        db.start_transaction()
-        try:
-            for code, currency in rs:
-                print str.format('Fetching for currency data for {0}...', currency)
-                data = cm.get_data(url=str.format('http://download.finance.yahoo.com/d/quotes.csv?s={0}CNY=X'
-                                                  '&f=sl1d1t1ba&e=.json', currency))
-                rdr = csv.reader(StringIO(data['body']))
-                line_data = [val for val in rdr][0]
-                timestamp = datetime.datetime.strptime(str.format('{0} {1}', line_data[2], line_data[3]),
-                                                       '%m/%d/%Y %I:%M%p')
-                db.update({'rate': line_data[1], 'update_time': timestamp.strftime('%Y-%m-%d %H:%M:%S')},
-                          'region_info', str.format('iso_code="{0}"', code))
-            db.commit()
-        except:
-            db.rollback()
-            raise
+        with RoseVisionDb(getattr(gs, 'DB_SPEC')) as db:
+            rs = db.query_match(['iso_code', 'currency'], 'region_info').fetch_row(maxrows=0)
+            db.start_transaction()
+            try:
+                for code, currency in rs:
+                    print str.format('Fetching for currency data for {0}...', currency)
+                    data = cm.get_data(url=str.format('http://download.finance.yahoo.com/d/quotes.csv?s={0}CNY=X'
+                                                      '&f=sl1d1t1ba&e=.json', currency))
+                    rdr = csv.reader(StringIO(data['body']))
+                    line_data = [val for val in rdr][0]
+                    timestamp = datetime.datetime.strptime(str.format('{0} {1}', line_data[2], line_data[3]),
+                                                           '%m/%d/%Y %I:%M%p')
+                    db.update({'rate': line_data[1], 'update_time': timestamp.strftime('%Y-%m-%d %H:%M:%S')},
+                              'region_info', str.format('iso_code="{0}"', code))
+                db.commit()
+            except:
+                db.rollback()
+                raise
 
 # if __name__ == '__main__':
 #     t= CurrencyUpdate()
