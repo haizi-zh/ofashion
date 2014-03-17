146f89efda0eeccfcb6e302363dd0ce8c96cc740
Haizi Zheng <haizi.zh@gmail.com>
2014-03-17 16:31:11 +0800

    FIX BUG: BACKUP
    备份数据库的时候，查找schema中所有数据表，然后进行备份。

--- utils/backup.py
+++ utils/backup.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import datetime
+from core import RoseVisionDb
 import global_settings as glob
 import os
 from utils.utils_core import get_logger
@@ -10,7 +11,6 @@ __author__ = 'Zephyre'
 class BackupTasker(object):
     @classmethod
     def run(cls, logger=None, **kwargs):
-        logger=None
         if not logger:
             logger = get_logger(to_file=True)
 
@@ -19,22 +19,21 @@ class BackupTasker(object):
         try:
             db_spec = getattr(glob, 'DATABASE')[kwargs['DATABASE']]
             # {"host": "127.0.0.1", "port": 1228, "schema": "editor_stores", "username": "rose", "password": "rose123"}
-            host=db_spec['host']
-            port=db_spec['port'] if 'port' in db_spec else 3306
-            db = db_spec['schema']
+            host = db_spec['host']
+            port = db_spec['port'] if 'port' in db_spec else 3306
+            schema = db_spec['schema']
             db_user = db_spec['username']
             db_pwd = db_spec['password']
 
-            ssh_user, ssh_host, ssh_port, dst = (None,None,22, '')
+            ssh_user, ssh_host, ssh_port, dst = (None, None, 22, '')
             if 'SSH_USER' in kwargs:
-                ssh_user=kwargs['SSH_USER']
+                ssh_user = kwargs['SSH_USER']
             if 'SSH_HOST' in kwargs:
                 ssh_host = kwargs['SSH_HOST']
             if 'SSH_PORT' in kwargs:
                 ssh_port = int(kwargs['SSH_PORT'])
             if 'SSH_DST' in kwargs:
                 dst = kwargs['SSH_DST']
-
         except (AttributeError, KeyError):
             logger.exception('Invalid database specification.')
             return
@@ -42,11 +41,20 @@ class BackupTasker(object):
         host_str = str.format('-h{0}', host) if host else ''
         port_str = str.format('-P{0}', port) if port else ''
 
-        tables = ['brand_info', 'brand_duration', 'translation', 'region_info', 'images_store', 'mfashion_tags',
-                  'original_tags', 'products', 'products_image', 'products_price_history', 'products_release']
+        # 获得schema中的所有table
+        with RoseVisionDb(getattr(glob, 'DB_SPEC')) as db:
+            tables = [tmp[0] for tmp in db.query(
+                str.format('select TABLE_SCHEMA from information_schema.tables where TABLE_SCHEMA="{0}"',
+                           schema)).fetch_row(maxrows=0)]
+
+        # tables = ['brand_info', 'brand_duration', 'translation', 'region_info', 'images_store', 'mfashion_tags',
+        #           'original_tags', 'products', 'products_image', 'products_mfashion_tags', 'products_original_tags',
+        #           'products_price_history', 'products_release', 'products_translate']
         for table in tables:
             logger.info(str.format('EXPORTING {0}...', table))
-            os.system(str.format('mysqldump {3} {4} -u {0} -p{1} -c {2} {5} > /tmp/{5}.sql', db_user, db_pwd, db, host_str, port_str, table))
+            os.system(
+                str.format('mysqldump {3} {4} -u {0} -p{1} -c {2} {5} > /tmp/{5}.sql', db_user, db_pwd, schema,
+                           host_str, port_str, table))
 
         # 将所有的sql文件打包
         logger.info('ZIPPING...')
@@ -65,6 +73,7 @@ class BackupTasker(object):
             # 指明了SSH信息，需要上传到远程服务器作为备份
             logger.info('UPLOADING...')
             ssh_port_str = str.format('-P {0}', ssh_port) if ssh_port else ''
-            os.system(str.format('scp {0} {4} {1}@{2}:{3} > /dev/null', ssh_port_str, ssh_user, ssh_host, dst, backup_name))
+            os.system(
+                str.format('scp {0} {4} {1}@{2}:{3} > /dev/null', ssh_port_str, ssh_user, ssh_host, dst, backup_name))
 
         logger.info(str.format('AUTO BACKUP COMPLETED: {0}', backup_name))
