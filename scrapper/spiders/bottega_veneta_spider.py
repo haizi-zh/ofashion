3d9e5e69f6be60dba966f9c1b00484c6c24f4927
dhcdht <dhcdht@qq.com>
2014-04-08 15:06:27 +0800

    bottega_veneta增加sold_out判断为下线

--- scrapper/spiders/bottega_veneta_spider.py
+++ scrapper/spiders/bottega_veneta_spider.py
@@ -143,10 +143,23 @@ class BottegaSpider(MFashionSpider):
         yield item
 
     @classmethod
+    def fetch_other_offline_identifier(cls, response, spider=None):
+        sel = Selector(response)
+
+        soldout_node = sel.xpath('//div[@id="itemContent"]//div[@id="soldOut"]')
+
+        if soldout_node:
+            return True
+        else:
+            return False
+
+    @classmethod
     def is_offline(cls, response, spider=None):
         model = cls.fetch_model(response)
 
-        if model:
+        other_offline_identifier = cls.fetch_other_offline_identifier(response, spider)
+
+        if model and not other_offline_identifier:
             return False
         else:
             return True
