5a888fc7ba9e2cb3d16955f3cc6ef4484263137a
dhcdht <dhcdht@qq.com>
2014-04-08 14:55:40 +0800

    bally增加sold_out判断为下线

--- scrapper/spiders/bally_spider.py
+++ scrapper/spiders/bally_spider.py
@@ -250,11 +250,24 @@ class BallySpider(MFashionSpider):
         yield item
 
     @classmethod
+    def fetch_other_offline_identifier(cls, response, spider=None):
+        sel = Selector(response)
+
+        soldout_node = sel.xpath('//div[@id="wrapper"]//div[@id="cartBox"]//a[@id="linkAddToCart"][contains(@href, "emptyFunction()")]')
+
+        if soldout_node:
+            return True
+        else:
+            return False
+
+    @classmethod
     def is_offline(cls, response, spider=None):
         model = cls.fetch_model(response)
         name = cls.fetch_name(response)
 
-        if model and name:
+        other_offline_identifier = cls.fetch_other_offline_identifier(response, spider)
+
+        if model and name and not other_offline_identifier:
             return False
         else:
             return True
