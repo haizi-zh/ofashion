336b166c64d24cd1243035b754b1109ca21ce9eb
dhcdht <dhcdht@qq.com>
2014-04-09 15:29:39 +0800

    ysl增加sold_out判断为下线

--- scrapper/spiders/ysl_spider.py
+++ scrapper/spiders/ysl_spider.py
@@ -205,10 +205,23 @@ class YslSpider(MFashionSpider):
         yield item
 
     @classmethod
+    def fetch_other_offline_identifier(cls, response, spider=None):
+        sel = Selector(response)
+
+        soldout_node = sel.xpath('//div[@id="itemContent"]/div[@id="itemDetails"]/div[@class="soldOut"][text()]')
+
+        if soldout_node:
+            return True
+        else:
+            return False
+
+    @classmethod
     def is_offline(cls, response, spider=None):
         model = cls.fetch_model(response)
 
-        if model:
+        other_offline_identifier = cls.fetch_other_offline_identifier(response, spider)
+
+        if model and not other_offline_identifier:
             return False
         else:
             return True
