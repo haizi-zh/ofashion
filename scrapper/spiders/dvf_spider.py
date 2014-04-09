0a3d348a3d6c10f4013822c6b6cd301e8b187583
dhcdht <dhcdht@qq.com>
2014-04-09 17:47:18 +0800

    dvf增加sold_out判断为下线

--- scrapper/spiders/dvf_spider.py
+++ scrapper/spiders/dvf_spider.py
@@ -252,10 +252,23 @@ class DVFSpider(MFashionSpider):
         return ret
 
     @classmethod
+    def fetch_other_offline_identifier(cls, response, spider=None):
+        sel = Selector(response)
+
+        soldout_node = sel.xpath('//div[@id="product-content"]//div[@class="oos-mssg"]')
+
+        if soldout_node:
+            return True
+        else:
+            return False
+
+    @classmethod
     def is_offline(cls, response, spider=None):
         model = cls.fetch_model(response)
 
-        if model:
+        other_offline_identifier = cls.fetch_other_offline_identifier(response, spider)
+
+        if model and not other_offline_identifier:
             return False
         else:
             return True
