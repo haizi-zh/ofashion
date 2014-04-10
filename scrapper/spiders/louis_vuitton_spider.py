b5b813d26857de6ceec14d65dbbcc588c0b12ecf
dhcdht <dhcdht@qq.com>
2014-04-10 14:21:04 +0800

    lv增加了有notinstock按钮判断为下线

--- scrapper/spiders/louis_vuitton_spider.py
+++ scrapper/spiders/louis_vuitton_spider.py
@@ -285,10 +285,23 @@ class LouisVuittonSpider(MFashionSpider):
         yield item
 
     @classmethod
+    def fetch_other_offline_identifier(cls, response, spider=None):
+        sel = Selector(response)
+
+        notinstock_node = sel.xpath('//div[@id="infoProductBlock"]//div[@id="notInStock"]')
+
+        if notinstock_node:
+            return True
+        else:
+            return False
+
+    @classmethod
     def is_offline(cls, response, spider=None):
         model = cls.fetch_model(response)
 
-        if model:
+        other_offline_identifier = cls.fetch_other_offline_identifier(response, spider)
+
+        if model and not other_offline_identifier:
             return False
         else:
             return True
