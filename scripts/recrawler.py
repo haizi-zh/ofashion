6aa92d2f08fc1cdb7a8dacb72c95b224472c4e81
jingwei.zheng <jingwei.zheng@qq.com>
2014-05-06 11:58:50 +0800

    changed recrawler.py for DKNY, need to modify

--- scripts/recrawler.py
+++ scripts/recrawler.py
@@ -15,12 +15,17 @@ parameter = {'idmonitor': idm, 'brand_id': brand, 'region': region}
 with RoseVisionDb(getattr(gs, 'DATABASE')['DB_SPEC']) as db:
     db.update({'monitor_status': 0, 'monitor_pid': None, 'recrawl_pid': os.getpid()}, 'monitor_status',
               str.format('idmonitor={0}', parameter['idmonitor']))
-
-os.system('python %s update --brand %s -r %s' % (
-    os.path.join(scripts.__path__[0], 'run_crawler.py'), parameter['brand_id'], parameter['region']))
-os.system(
-    'python %s %s -r %s' % (
+#todo hardcode for DKNY, need to add 'is_offline' for DknySpider
+if brand == 10108:
+    os.system(
+        'python %s %s -r %s' % (
+            os.path.join(scripts.__path__[0], 'run_crawler.py'), parameter['brand_id'], parameter['region']))
+else:
+    os.system('python %s update --brand %s -r %s' % (
         os.path.join(scripts.__path__[0], 'run_crawler.py'), parameter['brand_id'], parameter['region']))
+    os.system(
+        'python %s %s -r %s' % (
+            os.path.join(scripts.__path__[0], 'run_crawler.py'), parameter['brand_id'], parameter['region']))
 # os.system('python %s process-tags --cond brand_id=%s' % parameter['brand_id'])
 # os.system('python %s release --brand %s' % parameter['brand_id'])
 
